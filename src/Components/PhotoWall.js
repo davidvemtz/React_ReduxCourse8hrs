145607007333f959ed64475d230725133e5cb7fd
David Velasco Martinez <davidve@amdocs.com>
2019-11-03 16:02:55 -0600

    first commit to the project

---
+++ src/Components/PhotoWall.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Photo from './Photo';
+import PropTypes from 'prop-types';
+
+function PhotoWall(props) {
+    return  <div className='photoGrid'>
+                {props.posts.map((post,index) => <Photo key = {index} post = {post} onRemovePhoto={props.onRemovePhoto}/>)}
+            </div>
+}
+
+PhotoWall.propTypes = {
+    posts: PropTypes.array.isRequired,
+    onRemovePhoto: PropTypes.func.isRequired
+}
+/* 
+class PhotoWall extends Component {
+    render() {
+        return  <div className='photoGrid'>
+                {this.props.posts.map((post,index) => <Photo key = {index} post = {post}/>)}
+                </div>
+    }
+} */
+
+export default PhotoWall;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import Main from './Components/Main';
+import './styles/stylesheet.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// const tasks = ['take out trash', 'shovel the driveway', 'walk my dog'];
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+/* const element = React.createElement('ol', null, 
+    tasks.map((task,index) => React.createElement('li', {key: index}, task))
+); */
+
+/* const element = 
+    <div>
+        <h1> Tasks List </h1>
+        <ol> 
+            {tasks.map((task, index) => <li key = {index}>{task}</li>)}
+        </ol>
+    </div> */
+
+ReactDOM.render(<Main/>, document.getElementById('root'));
