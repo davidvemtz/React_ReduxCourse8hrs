c9569af16d886063a9173ace22d5648f74480e36
DavidVMartinez <David@MacPro-DavidVMtz.local>
2019-11-03 21:19:36 -0600

    second commit, until Link video, added Link before using react routes to display UI's

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Title from './Title';
 import PhotoWall from './PhotoWall';
+import AddPhoto from './AddPhoto';
+import {Route} from 'react-router-dom';
 
 class Main extends Component {
     constructor() {
@@ -11,18 +13,20 @@ class Main extends Component {
                 description: "beautiful landscape",
                 imageLink: "https://image.jimcdn.com/app/cms/image/transf/none/path/sa6549607c78f5c11/image/i4eeacaa2dbf12d6d/version/1490299332/most-beautiful-landscapes-in-europe-lofoten-european-best-destinations-copyright-iakov-kalinin.jpg" +
                     "3919321_1443393332_n.jpg"
-                }, {
+            }, {
                 id: "1",
                 description: "Aliens???",
                 imageLink: "https://img.purch.com/rc/640x415/aHR0cDovL3d3dy5zcGFjZS5jb20vaW1hZ2VzL2kvMDAwLzA3Mi84NTEvb3JpZ2luYWwvc3BhY2V4LWlyaWRpdW00LWxhdW5jaC10YXJpcS1tYWxpay5qcGc=" +
                     "08323785_735653395_n.jpg"
-                }, {
+            }, {
                 id: "2",
                 description: "On a vacation!",
                 imageLink: "https://fm.cnbc.com/applications/cnbc.com/resources/img/editorial/2017/08/24/104670887-VacationExplainsTHUMBWEB.1910x1000.jpg"
-            }]
+            }],
+            screen: 'photos' // photos, addPhoto
         };
         this.removePhoto = this.removePhoto.bind(this);
+        this.navigate = this.navigate.bind(this);
     }
 
     removePhoto(postRemoved) {
@@ -32,12 +36,38 @@ class Main extends Component {
         }))
     };
 
-    render() {
-        return  <div>
-                    <Title title={'Photowall'} />
-                    <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto}/>
-                </div>
+    navigate() {
+        this.setState({
+            screen: 'addPhoto'
+        })
+    }
+
+    componentDidMount() {
+        // const data = SimulateFetchData();
+        // this.setState({
+        //     posts: data
+        // })
+        console.log('componentDidMount');
     };
+
+    componentDidUpdate(prevProps, prevState) {
+        console.log('componentDidUpdate');
+        console.log(prevState.posts);
+        console.log(this.state);
+    }
+
+    render() {
+        return (
+            <div>
+                <Route path='/' render={() => (
+                    <div>
+                        <Title title={'Photowall'} />
+                        <PhotoWall posts={this.state.posts} onRemovePhoto={this.removePhoto}  onNavigate={this.navigate}/>
+                    </div>               
+            )}/>
+                <Route path="/AddPhoto" component={AddPhoto}/>
+            </div> 
+        )};
 };
 
 export default Main;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,7 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import Main from './Components/Main';
 import './styles/stylesheet.css';
+import {BrowserRouter} from 'react-router-dom';
 
-// const tasks = ['take out trash', 'shovel the driveway', 'walk my dog'];
+ReactDOM.render(<BrowserRouter><Main/></BrowserRouter>, document.getElementById('root'));
 
-/* const element = React.createElement('ol', null, 
-    tasks.map((task,index) => React.createElement('li', {key: index}, task))
-); */
-
-/* const element = 
-    <div>
-        <h1> Tasks List </h1>
-        <ol> 
-            {tasks.map((task, index) => <li key = {index}>{task}</li>)}
-        </ol>
-    </div> */
-
-ReactDOM.render(<Main/>, document.getElementById('root'));
